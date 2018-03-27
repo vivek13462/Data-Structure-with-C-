0848b83d78f42dd56e25b9afa9c36903330c4ba0
vivek13462 <vivek.13462@gmail.com>
2018-03-27 10:44:50 -0700

    Reverse Sentence logic in java

---
+++ ReverseSentence.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ReverseSentance {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a sentance");
+		String inputString = sc.nextLine();
+
+		String[] words = inputString.split(" ");
+
+		for (int i = words.length-1 ; i >= 0; i--) {
+			System.out.print(words[i] +" ");
+		}
+	}
+}
