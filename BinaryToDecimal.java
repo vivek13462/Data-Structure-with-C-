d2af5206da1788daffa66dfe05228098631244e0
vivek13462 <vivek.13462@gmail.com>
2018-03-27 10:53:41 -0700

    Binary To Decimal logic in java

---
+++ BinaryToDecimal.java
@@ -0,0 +1,17 @@
+public class BinaryToDecimal {
+
+	public static void main(String[] args) {
+		String binaryNumber = "110010";
+		int decimal = 50;
+		int binary =110010;
+		
+		int decimalValue = Integer.parseInt(binaryNumber, 2);
+		
+		String hexa = Integer.toHexString(decimal);
+		
+		System.out.println(decimalValue);
+		System.out.println(hexa);
+		
+	}
+
+}
